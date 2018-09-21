0dca4ba7ffdca776a7369cc238a181fa636dc182
Bhumika Mistry <mistrybhk@gmail.com>
2018-09-21 18:03:40 +0100

    added placeholder to field

--- view/youtube_download/youtube_frame.py
+++ view/youtube_download/youtube_frame.py
@@ -14,10 +14,11 @@ class YoutubeFrame(Tk.Frame):
         Tk.Frame.__init__(self, parent, background="black")
         self._event_handler = event_handler
 
-
         # Create gui components and pack them
-        url_entry = Tk.Entry(self)
+        url_entry = EntryWithPlaceholder(self, "Enter YouTube URL")
         int_var = Tk.IntVar()
+        # Set to be ticked
+        int_var.set(1)
         play_checkbox = Tk.Checkbutton(self, text="Auto-play", variable=int_var)
         download_button = Tk.Button(self, text="Download",
                                     command=partial(self.event_handler.on_youtube_download,
@@ -31,3 +32,30 @@ class YoutubeFrame(Tk.Frame):
     @property
     def event_handler(self):
         return self._event_handler
+
+
+class EntryWithPlaceholder(Tk.Entry):
+    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
+        super().__init__(master)
+
+        self.placeholder = placeholder
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.foc_in)
+        self.bind("<FocusOut>", self.foc_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self):
+        self.insert(0, self.placeholder)
+        self['fg'] = self.placeholder_color
+
+    def foc_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('0', 'end')
+            self['fg'] = self.default_fg_color
+
+    def foc_out(self, *args):
+        if not self.get():
+            self.put_placeholder()
